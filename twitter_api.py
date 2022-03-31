cb4689747d2e4a0d28eadae4be5c4a93bfc12a2a
minkichae <99165394+minkichae@users.noreply.github.com>
2022-03-30 21:43:54 -0400

    Add files via upload

---
+++ twitter_api.py
@@ -0,0 +1,28 @@
+import tweepy
+import configparser
+import pandas as pd
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+
+access_token = config['twitter']['access_token']
+access_token_secret = config['twitter']['access_token_secret']
+
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+
+columns = ['Time', 'User', 'Tweet']
+data = []
+for tweet in public_tweets:
+    data.append([tweet.created_at, tweet.user.screen_name, tweet.text])
+
+df = pd.DataFrame(data, columns = columns)
+
+print(df)
